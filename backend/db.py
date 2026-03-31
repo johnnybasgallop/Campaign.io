3dd224fe57cd6fd8b78f8520c6304965c129331b
johnnybasgallop <johnnybasgallopwork@outlook.com>
2026-03-31 02:58:18 +0100

    adjustd connection pool size

--- backend/db.py
+++ backend/db.py
@@ -14,6 +14,8 @@ async def init_pool():
         database=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASS"),
+        min_size=1,
+        max_size=5,
     )
 
 
